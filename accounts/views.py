758008cc2907bd0d1e8c21a113f21d2732d23a7a
siesto1elemento <rohitojha9720@gmail.com>
2024-01-17 13:53:01 +0530

    Added login and logout feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserCreateForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,logout,authenticate
 from django.shortcuts import redirect
+
+
+def logoutaccount(request):
+    logout(request)
+    return redirect('home')
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html',{'form':AuthenticationForm})
+    else:
+        user = authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        if user is None:
+            return render(request,'loginaccount.html',{'form': AuthenticationForm(),'error': 'username and password donot match'})
+        else:
+            login(request,user)
+            return redirect('home')
+
+
 
 
 def signupaccount(request):
     if request.method == 'GET':
-        return render(request, 'signupaccount.html',{'form':UserCreationForm})
+        return render(request, 'signupaccount.html',{'form':UserCreateForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
             user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
@@ -15,4 +34,4 @@ def signupaccount(request):
             login(request, user)
             return redirect('home')
         else:
-            return render(request, 'signupaccount.html',{'form':UserCreationForm,'error':'Passwords do not match'})
+            return render(request, 'signupaccount.html',{'form':UserCreateForm,'error':'Passwords do not match'})
